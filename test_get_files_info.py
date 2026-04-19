1ae871259b48e487afffd4155c12aad21cd0c102
Sherezz Grant <rezziemaven@gmail.com>
2026-04-19 12:09:04 -0400

    Remove comments

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,13 +1,5 @@
 from functions.get_files_info import get_files_info
 
-# print(get_files_info("calculator", "."))
-
-# print(get_files_info("calculator", "pkg"))
-
-# print(get_files_info("calculator", "/bin"))
-
-# print(get_files_info("calculator", "../"))
-
 test_cases = [
     ("calculator", "."),
     ("calculator", "pkg"),
@@ -17,7 +9,6 @@ test_cases = [
 
 for tc in test_cases:
     print("==================================")
-    # print("----------------------------------")
     print(f"Listing contents of {tc[1]} in {tc[0]}...")
     print(get_files_info(*tc))
     print(" ")
