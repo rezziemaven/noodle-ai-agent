c57bf92986af03b3ce969fe67a532cc8124b1227
Sherezz Grant <rezziemaven@gmail.com>
2026-04-20 08:54:33 -0400

    Import sdks into main

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import argparse
 import os
 
 from dotenv import load_dotenv
-from google import genai
-from google.genai import types
-from openrouter import OpenRouter
+
+from sdks.genai import use_genai_sdk
+from sdks.ollama import use_ollama_sdk
+from sdks.openrouter import use_openrouter_sdk
 
 
 def main():
-    # print("Hello from ai-agent!")
     load_dotenv()
 
     gemini_api_key = os.environ.get("GEMINI_API_KEY")
@@ -22,14 +22,11 @@ def main():
         raise RuntimeError(
             "OpenRouter API key not found. Please add key to your .env file."
         )
-
-    llm_model = os.environ.get("LLM_MODEL")
-    if llm_model == None:
-        llm_model = "google/gemini-2.5-flash"
-
-    base_url = os.environ.get("OPENROUTER_BASE_URL")
-    if base_url == None:
-        base_url = "https://openrouter.ai/api/v1"
+    ollama_api_key = os.environ.get("OLLAMA_API_KEY")
+    if ollama_api_key == None:
+        print(
+            "Ollama API key not found but optional. Please add key to your .env file for cloud model usage."
+        )
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
@@ -40,56 +37,28 @@ def main():
     )
     args = parser.parse_args()
 
-    def use_genai_sdk():
-        client = genai.Client(api_key=gemini_api_key)
-        messages = [
-            types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
-        ]
-        response = client.models.generate_content(
-            model="gemini-2.5-flash", contents=messages
-        )
+    def generate_response(sdk="ollama"):
+        if sdk == "gemini":
+            return use_genai_sdk(gemini_api_key, args.user_prompt, verbose=args.verbose)
 
-        prompt_token_count = 0
-        response_token_count = 0
-
-        if response.usage_metadata != None:
-            prompt_token_count = response.usage_metadata.prompt_token_count
-            response_token_count = response.usage_metadata.candidates_token_count
-
-        return prompt_token_count, response_token_count, response.text
-
-    def use_openrouter_sdk():
-        client = OpenRouter(api_key=openrouter_api_key)
-        messages = [{"role": "user", "content": args.user_prompt}]
-        response = client.chat.send(model=llm_model, messages=messages)
-
-        prompt_token_count = 0
-        response_token_count = 0
-
-        if response.usage != None:
-            prompt_token_count = response.usage.prompt_tokens
-            response_token_count = response.usage.completion_tokens
-
-        return (
-            prompt_token_count,
-            response_token_count,
-            response.choices[0].message.content,
-        )
-
-    def generate_response(sdk="openrouter"):
         if sdk == "openrouter":
-            return use_openrouter_sdk()
+            return use_openrouter_sdk(
+                openrouter_api_key, args.user_prompt, verbose=args.verbose
+            )
 
-        return use_genai_sdk()
+        return use_ollama_sdk(args.user_prompt, verbose=args.verbose)
 
-    prompt_token_count, response_token_count, response_text = generate_response()
+    prompt_token_count, response_token_count, response_text, function_results = (
+        generate_response()
+    )
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {prompt_token_count}")
-        print(f"Response tokens: {response_token_count}")
-    print("Response:")
-    print(response_text)
+    if len(function_results) == 0:
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {prompt_token_count}")
+            print(f"Response tokens: {response_token_count}")
+        print("Response:")
+        print(response_text)
 
 
 if __name__ == "__main__":
